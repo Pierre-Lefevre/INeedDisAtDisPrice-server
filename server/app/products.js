fe2994447ac31447cd8d6fe94220c7668d6e3e97
Pierre-Lefevre <lefevre.pierre.m.d@gmail.com>
2018-02-13 00:22:06 +0100

    Add price filter.

--- server/app/products.js
+++ server/app/products.js
@@ -6,21 +6,45 @@ const express = require('express'),
   Products = require('../models/Products.js')
 
 router.get('/api/products', async function (req, res) {
+
   let query = {}
   let page = req.query.page && typeof parseInt(req.query.page) === 'number' ? req.query.page : 1
   let pageSize = 40
 
-  let splitSearch = req.query.search.toLowerCase().split(' ')
-
-  let regex = ''
-  splitSearch.forEach(word => {
-    regex += '(?=.*\\b' + word + '\\b)'
-  })
-  regex += '.*'
-
   if (req.query.search) {
+    let splitSearch = req.query.search.toLowerCase().split(' ')
+    let regex = ''
+    splitSearch.forEach(word => {
+      regex += '(?=.*' + word + ')'
+    })
+    regex += '.*'
     query.name = {$regex: regex, $options: 'i'}
   }
+
+  let price = {}
+  if (req.query.minPrice && parseInt(req.query.minPrice) !== -1) {
+    price.$gt = parseInt(req.query.minPrice)
+  }
+  if (req.query.maxPrice && parseInt(req.query.maxPrice) !== -1) {
+    price.$lt = parseInt(req.query.maxPrice)
+  }
+  if (Object.keys(price).length > 0) {
+    query.price = price
+  }
+
+  let minMaxPrice = await Products.aggregate([
+    {$match: query},
+    {
+      '$group': {
+        '_id': null,
+        'max': {'$max': '$price'},
+        'min': {'$min': '$price'}
+      }
+    }
+  ])
+
+  let min = parseInt(req.query.minPrice) !== -1 ? parseInt(req.query.minPrice) : minMaxPrice.length > 0 ? Math.floor(minMaxPrice[0].min) : -1
+  let max = parseInt(req.query.maxPrice) !== -1 ? parseInt(req.query.maxPrice) : minMaxPrice.length > 0 ? Math.ceil(minMaxPrice[0].max) : -1
 
   let products = await Products.find(query).skip(pageSize * (page - 1)).limit(pageSize)
   let nbPage = Math.ceil(await Products.find(query).count() / pageSize)
@@ -30,15 +54,16 @@ router.get('/api/products', async function (req, res) {
       products[i].image_name = 'default'
     }
 
-    let minPrice = product.price
-    let maxPrice = product.price
+    let minPrice = -1
+    let maxPrice = -1
 
     let similarProducts = await Products.find({'_id': {$in: product.similarities}})
 
     await utils.asyncForEach(similarProducts, async (similarProduct) => {
-      if (similarProduct.price < minPrice) {
+      if (similarProduct.price < minPrice || minPrice === -1) {
         minPrice = similarProduct.price
-      } else if (similarProduct.price > minPrice) {
+      }
+      if (similarProduct.price > maxPrice || maxPrice === -1) {
         maxPrice = similarProduct.price
       }
     })
@@ -47,7 +72,7 @@ router.get('/api/products', async function (req, res) {
     product.maxPrice = maxPrice
   })
 
-  res.json({products, nbPage})
+  res.json({products, nbPage, min, max})
 })
 
 router.get('/api/product/:id', async function (req, res) {
